5ac4221434a9031dd1e5641ec87838ca74ae378c
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-03-02 18:47:05 -0300

    subindo o arquivo de configuração

---
+++ source/Config.php
@@ -0,0 +1,32 @@
+<?php
+
+define("BASE_URL", "http://localhost/PHPtest");
+
+define("DATA_LAYER_CONFIG", [
+  "driver" => "mysql",
+  "host" => "localhost",
+  "port" => "3306",
+  "dbname" => "cd2tec",
+  "username" => "root",
+  "passwd" => "",
+  "options" => [
+      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+      PDO::ATTR_CASE => PDO::CASE_NATURAL
+  ]
+]);
+
+
+
+/**
+ * Helper
+ */
+function url(string $uri = null)
+{
+  if($uri) {
+    return BASE_URL . "/{$uri}";
+  }
+
+  return BASE_URL;
+}
